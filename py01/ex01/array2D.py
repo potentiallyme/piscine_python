c2efb5397fd3396108140500d88742d954744853
Loic Moran <112955036+potentiallyme@users.noreply.github.com>
2025-05-05 10:57:10 +0200

    finished py01/ex01, normed and tested

---
+++ py01/ex01/array2D.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def slice_me(family: list, start: int, end: int) -> list:
+    '''
+    Takes a list and slices it into the given size
+
+    Args:
+        family (list): original list to slice
+        start (int): starting index
+        end (int): ending index
+
+    Returns:
+        List created from sliced array
+    '''
+    try:
+        assert isinstance(family, list), '1st param must be list'
+        a = np.array(family)
+        assert np.issubdtype(a.dtype, np.number), 'list must contain nums'
+        print(f'My shape is: {a.shape}')
+        b = a[start:end]
+        print(f'My new shape is: {b.shape}')
+        return b.tolist()
+    except AssertionError as e:
+        print('AssertionError:', e)
